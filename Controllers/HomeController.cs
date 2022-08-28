9d34c4bf2772a948a34546a3fd064b8c7a7f0132
Bharat001 <bharat.honor2@hotmail.com>
2022-08-28 11:16:51 +0530

    HomeAdded

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationMicreoservice3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
